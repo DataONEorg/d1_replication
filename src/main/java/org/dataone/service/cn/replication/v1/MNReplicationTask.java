35adcc537d0c8c1ad089c1e4a1885cd9a9345d7b
Chris Jones <cjones@nceas.ucsb.edu>
2012-01-17 03:44:22 +0000

    Add svn keywords to the Task classes for replication.

--- src/main/java/org/dataone/service/cn/replication/v1/MNReplicationTask.java
+++ src/main/java/org/dataone/service/cn/replication/v1/MNReplicationTask.java
@@ -17,7 +17,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * $Id: $
+ * $Id$
  */
 package org.dataone.service.cn.replication.v1;
 
