62c39d99d6eb3534ea5b84b53ea9914684fd3f36
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-10-25 19:59:21 +0000

    using configuration flag to toggle whether system metadata events trigger creation of replication events.

--- src/main/java/org/dataone/service/cn/replication/v1/ReplicationEventListener.java
+++ src/main/java/org/dataone/service/cn/replication/v1/ReplicationEventListener.java
@@ -42,18 +42,21 @@ import com.hazelcast.core.ItemListener;
 
 /**
  * An event listener used to manage change events on the hzSystemMetadata map.
- * The listener queues Identifiers into the hzReplicationEvents queue when the 
- * system metadata map is added to or updated.  The events queue is monitored
- * for additions, and the ReplicationManager is called to evaluate replication
+ * The listener queues Identifiers into the hzReplicationEvents queue when the
+ * system metadata map is added to or updated. The events queue is monitored for
+ * additions, and the ReplicationManager is called to evaluate replication
  * policies for the given identifier and potentially create replication tasks.
  * 
  * @author cjones
- *
+ * 
  */
-public class ReplicationEventListener 
-    implements EntryListener<Identifier, SystemMetadata>, ItemListener<Identifier> {
+public class ReplicationEventListener implements EntryListener<Identifier, SystemMetadata>,
+        ItemListener<Identifier> {
 
-    /* A prefix appended to an identifier for coordinated locks across ReplicationManagers */
+    /*
+     * A prefix appended to an identifier for coordinated locks across
+     * ReplicationManagers
+     */
     private static final String EVENT_PREFIX = "replication-event-";
 
     /* Get a Log instance */
@@ -64,37 +67,37 @@ public class ReplicationEventListener
 
     /* the instance of the Hazelcast processing cluster member */
     private HazelcastInstance hzMember;
-            
+
     /* The name of the replication events queue */
     private String eventsQueue;
-    
+
     /* The name of the system metadata map */
     private String systemMetadataMap;
-    
+
     /* The Hazelcast distributed system metadata map */
     private IMap<Identifier, SystemMetadata> systemMetadata;
-    
+
     /* The ReplicationManager instance */
     ReplicationManager replicationManager;
-    
-    /* The Hazelcast distributed replication events queue*/
+
+    /* The Hazelcast distributed replication events queue */
     private IQueue<Identifier> replicationEvents;
-        
+
     /**
      * Constructor: create a replication event listener that listens for entry
-     * events on the hzSystemMetadata map a queues the identifier key for 
+     * events on the hzSystemMetadata map a queues the identifier key for
      * replication task creation. Intended to be used by the ReplicationManager
-     * to manage hzSystemMetadata map events 
+     * to manage hzSystemMetadata map events
      */
     public ReplicationEventListener() {
         // connect to both the process and storage cluster
         this.hzClient = HazelcastClientInstance.getHazelcastClient();
         this.hzMember = Hazelcast.getDefaultInstance();
-        this.eventsQueue =
-            Settings.getConfiguration().getString("dataone.hazelcast.replicationQueuedEvents");
+        this.eventsQueue = Settings.getConfiguration().getString(
+                "dataone.hazelcast.replicationQueuedEvents");
         // get references to the system metadata map and events queue
-        this.systemMetadataMap =
-            Settings.getConfiguration().getString("dataone.hazelcast.systemMetadata");
+        this.systemMetadataMap = Settings.getConfiguration().getString(
+                "dataone.hazelcast.systemMetadata");
         this.systemMetadata = this.hzClient.getMap(systemMetadataMap);
         this.replicationEvents = this.hzMember.getQueue(eventsQueue);
 
@@ -103,34 +106,33 @@ public class ReplicationEventListener
         log.info("Added a listener to the " + this.systemMetadata.getName() + " map.");
         this.replicationEvents.addItemListener(this, true);
         log.info("Added a listener to the " + this.replicationEvents.getName() + " queue.");
-        
-      
+
     }
-    
+
     /**
      * Initialize the bean object
      */
     public void init() {
         log.info("initialization");
-        
+
     }
-    
+
     /**
-     * Listen for item added events on the hzReplicationEvents queue.  Call 
-     * the replicationManager to evaluate the replication policy for the identifier
+     * Listen for item added events on the hzReplicationEvents queue. Call the
+     * replicationManager to evaluate the replication policy for the identifier
      */
     public void itemAdded(Identifier identifier) {
-        log.info("Item added event received on the [end of] hzReplicationEvents queue for " 
-            + identifier.getValue());
+        log.info("Item added event received on the [end of] hzReplicationEvents queue for "
+                + identifier.getValue());
         Identifier pid = null;
         boolean isLocked = false;
         ILock lock = null;
         String lockPrefix = "handled-replication-events-";
-        
+
         try {
             // poll the queue to pop the most recent event off of the queue
             pid = this.replicationEvents.poll(3L, TimeUnit.SECONDS);
-            if ( pid != null ) {    
+            if (pid != null) {
                 log.info("Won the replication events queue poll [top of] for " + pid.getValue());
                 String lockString = lockPrefix + pid.getValue();
                 lock = this.hzMember.getLock(lockString);
@@ -138,169 +140,178 @@ public class ReplicationEventListener
                 isLocked = lock.tryLock(1L, TimeUnit.SECONDS);
                 if (isLocked) {
                     log.debug("Gained the event lock " + lockString);
-                    log.trace("METRICS:\tREPLICATION:\tEVALUATE:\tPID:\t"
-                            + pid.getValue());
-                    
-                    // evaluate the object's replication policy for potential task creation
+                    log.trace("METRICS:\tREPLICATION:\tEVALUATE:\tPID:\t" + pid.getValue());
+
+                    // evaluate the object's replication policy for potential
+                    // task creation
                     this.replicationManager.createAndQueueTasks(pid);
-                    
-                    log.trace("METRICS:\tREPLICATION:\tEND EVALUATE:\tPID:\t"
-                            + pid.getValue());
-                    
+
+                    log.trace("METRICS:\tREPLICATION:\tEND EVALUATE:\tPID:\t" + pid.getValue());
+
                 } else {
                     log.debug("Didn't gain the event lock " + lockString);
-                    
+
                 }
             }
-            
+
         } catch (BaseException e) {
-            log.error(
-                    "There was a problem handling task creation for "
-                            + pid.getValue() + ". The error message was "
-                            + e.getMessage(), e);
-            // something went very wrong trying to create tasks for this 
-            // pid.  Resubmit it to evaluate again.
+            log.error("There was a problem handling task creation for " + pid.getValue()
+                    + ". The error message was " + e.getMessage(), e);
+            // something went very wrong trying to create tasks for this
+            // pid. Resubmit it to evaluate again.
             queueEvent(pid);
-                            
+
         } catch (InterruptedException e) {
             log.error("Polling of the hzReplicationEvents queue was interrupted.", e);
-            
+
         } finally {
-            if ( isLocked ) {
+            if (isLocked) {
                 lock.unlock();
-                
+
             }
 
         }
     }
 
     /**
-     * Listen for item removed events on the hzReplicationEvents queue. 
+     * Listen for item removed events on the hzReplicationEvents queue.
      */
     public void itemRemoved(Identifier identifier) {
         // nothing to do
-        
+
+    }
+
+    private boolean replicationIsActive() {
+        return Boolean.parseBoolean(Settings.getConfiguration().getString("Replication.active"));
     }
 
     /**
-     * Implement the EntryListener interface, responding to entries being added to
-     * the hzSystemMetadata map.
+     * Implement the EntryListener interface, responding to entries being added
+     * to the hzSystemMetadata map.
      * 
-     * @param event - the entry event being added to the map
+     * @param event
+     *            - the entry event being added to the map
      */
     public void entryAdded(EntryEvent<Identifier, SystemMetadata> event) {
-        log.info("Received entry added event on the hzSystemMetadata map. Queueing " + 
-            event.getKey().getValue());
-                
-        // a lock to coordinate event handling between the 3 CN ReplicationManager instances
-        String lockString = EVENT_PREFIX + event.getKey().getValue();
-        Lock lock = null;
-        boolean isLocked = false;
-        
-        try {
-          
-            // lock the event string and queue the event. 
-            lock = this.hzMember.getLock(lockString);
-            isLocked = lock.tryLock(10L, TimeUnit.MILLISECONDS);
-            if (isLocked) {
-               log.info("Locked " + lockString);               
-               queueEvent(event.getKey());
-               lock.unlock();
-               log.info("Unlocked " + lockString);
-               isLocked = false;
-               
-            } else {
-                log.info("Didn't get lock for identifier " + event.getKey().getValue());
-                
+
+        if (replicationIsActive()) {
+            log.info("Received entry added event on the hzSystemMetadata map. Queueing "
+                    + event.getKey().getValue());
+
+            // a lock to coordinate event handling between the 3 CN
+            // ReplicationManager instances
+            String lockString = EVENT_PREFIX + event.getKey().getValue();
+            Lock lock = null;
+            boolean isLocked = false;
+
+            try {
+
+                // lock the event string and queue the event.
+                lock = this.hzMember.getLock(lockString);
+                isLocked = lock.tryLock(10L, TimeUnit.MILLISECONDS);
+                if (isLocked) {
+                    log.info("Locked " + lockString);
+                    queueEvent(event.getKey());
+                    lock.unlock();
+                    log.info("Unlocked " + lockString);
+                    isLocked = false;
+
+                } else {
+                    log.info("Didn't get lock for identifier " + event.getKey().getValue());
+
+                }
+
+            } catch (NullPointerException e) {
+                log.debug("The event identifier was null", e);
+            } catch (RuntimeException e) {
+                log.debug("Couldn't get a lock for " + lockString, e);
+            } catch (InterruptedException e) {
+                log.debug("Lock retreival was interrupted for " + lockString, e);
+            } finally {
+                if (isLocked) {
+                    lock.unlock();
+                    log.info("Unlocked " + lockString);
+
+                }
             }
-                  
-        } catch (NullPointerException e) {
-            log.debug("The event identifier was null", e);
-        } catch (RuntimeException e) {
-            log.debug("Couldn't get a lock for " + lockString, e);
-        } catch (InterruptedException e) {
-            log.debug("Lock retreival was interrupted for " + lockString, e);
-        } finally {
-            if (isLocked) {
-              lock.unlock();
-              log.info("Unlocked " + lockString);
-              
-            }           
-        }          
+        }
     }
 
-
     /**
-     * Implement the EntryListener interface, responding to entries being deleted from
-     * the hzSystemMetadata map.
+     * Implement the EntryListener interface, responding to entries being
+     * deleted from the hzSystemMetadata map.
      * 
-     * @param event - the entry event being deleted from the map
+     * @param event
+     *            - the entry event being deleted from the map
      */
     public void entryRemoved(EntryEvent<Identifier, SystemMetadata> event) {
-      // we don't remove replicas (do we?) 
-      
+        // we don't remove replicas (do we?)
+
     }
-    
 
     /**
-     * Implement the EntryListener interface, responding to entries being updated in
-     * the hzSystemMetadata map.
+     * Implement the EntryListener interface, responding to entries being
+     * updated in the hzSystemMetadata map.
      * 
-     * @param event - the entry event being updated in the map
+     * @param event
+     *            - the entry event being updated in the map
      */
     public void entryUpdated(EntryEvent<Identifier, SystemMetadata> event) {
-      
-        log.info("Received entry updated event on the hzSystemMetadata map. Queueing " + 
-            event.getKey().getValue());
-                    
-        // a lock to coordinate event handling between the 3 CN ReplicationManager instances
-        String lockString = EVENT_PREFIX + event.getKey().getValue();
-        Lock lock = null;
-        boolean isLocked = false;
-        
-        try {
-          
-            // lock the pid and queue the event. 
-            lock = this.hzMember.getLock(lockString);
-            isLocked = lock.tryLock(10L, TimeUnit.MILLISECONDS);
-            if (isLocked) {
-               log.info("Locked " + lockString);               
-               queueEvent(event.getKey());
-               log.info("Locked " + lockString);               
-               lock.unlock();
-               log.info("Unlocked " + lockString);
-               isLocked = false;
-               
-            } else {
-                log.info("Didn't get lock for identifier " + event.getKey().getValue());
-                
+
+        if (replicationIsActive()) {
+            log.info("Received entry updated event on the hzSystemMetadata map. Queueing "
+                    + event.getKey().getValue());
+
+            // a lock to coordinate event handling between the 3 CN
+            // ReplicationManager instances
+            String lockString = EVENT_PREFIX + event.getKey().getValue();
+            Lock lock = null;
+            boolean isLocked = false;
+
+            try {
+
+                // lock the pid and queue the event.
+                lock = this.hzMember.getLock(lockString);
+                isLocked = lock.tryLock(10L, TimeUnit.MILLISECONDS);
+                if (isLocked) {
+                    log.info("Locked " + lockString);
+                    queueEvent(event.getKey());
+                    log.info("Locked " + lockString);
+                    lock.unlock();
+                    log.info("Unlocked " + lockString);
+                    isLocked = false;
+
+                } else {
+                    log.info("Didn't get lock for identifier " + event.getKey().getValue());
+
+                }
+
+            } catch (NullPointerException e) {
+                log.debug("The event identifier was null", e);
+            } catch (RuntimeException e) {
+                log.debug("Couldn't get a lock for " + lockString, e);
+            } catch (InterruptedException e) {
+                log.debug("Lock retreival was interrupted for " + lockString, e);
+            } finally {
+                if (isLocked) {
+                    lock.unlock();
+                    log.info("Unlocked " + lockString);
+
+                }
             }
-                  
-        } catch (NullPointerException e) {
-            log.debug("The event identifier was null", e);
-        } catch (RuntimeException e) {
-            log.debug("Couldn't get a lock for " + lockString, e);
-        } catch (InterruptedException e) {
-            log.debug("Lock retreival was interrupted for " + lockString, e);
-        } finally {
-            if (isLocked) {
-              lock.unlock();
-              log.info("Unlocked " + lockString);
-              
-            }           
-        }                
+        }
     }
-    
 
     /**
-     * Implement the EntryListener interface, responding to entries being evicted from
-     * the hzSystemMetadata map.
+     * Implement the EntryListener interface, responding to entries being
+     * evicted from the hzSystemMetadata map.
      * 
-     * @param event - the entry event being evicted from the map
+     * @param event
+     *            - the entry event being evicted from the map
      */
     public void entryEvicted(EntryEvent<Identifier, SystemMetadata> event) {
-      // nothing to do, entry remains in backing store
-      
+        // nothing to do, entry remains in backing store
+
     }
 
     /*
@@ -309,45 +320,45 @@ public class ReplicationEventListener
      */
     private void queueEvent(Identifier identifier) {
         boolean added = false;
-        
+
         // add event identifiers only if they aren't already added
-        log.debug("The current number of potential replication events to be evaluated is: " 
-            + this.replicationEvents.size());
-        
-        // if it is not yet queued and not currently being handled, then we can add it
-        if ( !this.replicationEvents.contains(identifier) ) {
+        log.debug("The current number of potential replication events to be evaluated is: "
+                + this.replicationEvents.size());
+
+        // if it is not yet queued and not currently being handled, then we can
+        // add it
+        if (!this.replicationEvents.contains(identifier)) {
             added = this.replicationEvents.offer(identifier);
-            if ( added ) {
-                log.debug("Added " + identifier.getValue() + 
-                    " to the replication event queue");
-                
+            if (added) {
+                log.debug("Added " + identifier.getValue() + " to the replication event queue");
+
             } else {
-                log.debug("Failed to add " + identifier + 
-                " to the replication event queue");
+                log.debug("Failed to add " + identifier + " to the replication event queue");
 
             }
-        }                
+        }
     }
 
     /**
      * Get a reference to the ReplicationManager instance
      * 
-     * @return replicationManager - the singleton instance of the ReplicationManager
+     * @return replicationManager - the singleton instance of the
+     *         ReplicationManager
      */
     public ReplicationManager getReplicationManager() {
         return replicationManager;
-        
+
     }
 
     /**
      * Set the ReplicationManager instance
      * 
-     * param replicationManager - the singleton instance of the ReplicationManager
+     * param replicationManager - the singleton instance of the
+     * ReplicationManager
      */
     public void setReplicationManager(ReplicationManager replicationManager) {
         this.replicationManager = replicationManager;
-        
-    }
 
+    }
 
 }
